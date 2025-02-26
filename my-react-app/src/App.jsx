0ebef4ad80c9d166015092574a9a9200b98c0f3a
Basmahx <138460878+Basmahx@users.noreply.github.com>
2025-02-26 16:45:04 +0100

    création de react app P1

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import "./index.css";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
